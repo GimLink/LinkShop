ccfedd494341e5c1913f2637185b24a80f902a20
Link <mexlove2@gmail.com>
2024-05-24 16:57:00 +0900

    ModelMapper 설정

---
+++ src/main/java/com/linksang/LinkShop/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.linksang.LinkShop.config;
+
+import org.hibernate.collection.spi.PersistentCollection;
+import org.modelmapper.Condition;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setPropertyCondition(new Condition<Object, Object>() {
+            public boolean applies(MappingContext<Object, Object> context) {
+                return !(context.getSource() instanceof PersistentCollection<?>);
+            }
+        });
+        return mapper;
+    }
+}
