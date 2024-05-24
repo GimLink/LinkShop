31c90e7dbf41ff929336d20fc99e603f53535c59
Link <mexlove2@gmail.com>
2024-05-24 16:56:31 +0900

    AWS S3 설정

--- src/main/java/com/linksang/LinkShop/service/AwsS3Service.java
+++ src/main/java/com/linksang/LinkShop/service/AwsS3Service.java
@@ -1,10 +1,16 @@
 package com.linksang.LinkShop.service;
 
+
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,6 +19,25 @@ public class AwsS3Service {
 
     private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    String bucket;
+    @Value("${cloud.aws.s3.bucket")
+    private String bucket;
+    
+    public String upload(MultipartFile uploadFile, String fileName) throws IOException {
+        return putS3(uploadFile, fileName);
+    }
+
+    private String putS3(MultipartFile uploadFile, String fileName) throws IOException{
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile.getInputStream(), null));
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    public String getS3FileUrl(String filePath) {
+        return amazonS3Client.getUrl(bucket, filePath).toString();
+    }
+
+    public String createFileName(String fileName) {
+        return UUID.randomUUID().toString().substring(0, 13) + "_" + fileName;
+    }
+
 }
