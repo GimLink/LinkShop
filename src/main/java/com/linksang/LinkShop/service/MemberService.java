c4a16c516b77aa8126d1d3467f0993013758ec4f
Link <mexlove2@gmail.com>
2024-05-27 19:07:44 +0900

    회원 서비스 구현

---
+++ src/main/java/com/linksang/LinkShop/service/MemberService.java
@@ -0,0 +1,53 @@
+package com.linksang.LinkShop.service;
+
+import com.linksang.LinkShop.DTO.JoinMemberDto;
+import com.linksang.LinkShop.DTO.MemberDto;
+import com.linksang.LinkShop.entity.Item;
+import com.linksang.LinkShop.entity.Member;
+import com.linksang.LinkShop.enums.Sns;
+import org.springframework.data.domain.Pageable;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MemberService {
+
+    Model createJoinDtoErrorMsg(JoinMemberDto dto, Model model);
+
+    Map<String, String> getErrorMsg(Errors errors);
+
+    int joinValidationCheck(JoinMemberDto dto);
+
+    boolean existItem(Item item, String userId);
+
+    boolean existsByUserId(String userId);
+
+    boolean existsWithdrawalByUserId(String userId);
+
+    boolean checkAuthNum(String phoneNum, String authNum);
+
+    void setAuthCheck(String phoneNum);
+
+    void saveWithdrawalMember(String userId);
+
+    void login(String userId);
+
+    void withDrawal(String userId);
+
+    Member joinNormal(Member member);
+
+    Member joinOAuth(String id, Sns sns);
+
+    Member findByUserId(String userId);
+
+    Member findById(Long id);
+
+    Member getCurrentMember();
+
+    List<MemberDto> findAll(Pageable pageable);
+
+    List<String> findALlByPhoneNum(String phoneNum);
+
+}
---
+++ src/main/java/com/linksang/LinkShop/service/MemberServiceImpl.java
@@ -0,0 +1,4 @@
+package com.linksang.LinkShop.service;
+
+public class MemberServiceImpl {
+}
