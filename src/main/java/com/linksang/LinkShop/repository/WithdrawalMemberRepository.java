f5c4eecdc0bc8ac3f13a1b5114f11b6d40c732ed
Link <mexlove2@gmail.com>
2024-05-30 16:43:55 +0900

    탈퇴회원 레포

---
+++ src/main/java/com/linksang/LinkShop/repository/WithdrawalMemberRepository.java
@@ -0,0 +1,9 @@
+package com.linksang.LinkShop.repository;
+
+import com.linksang.LinkShop.entity.WithdrawalMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WithdrawalMemberRepository extends JpaRepository<WithdrawalMember, Long> {
+
+    boolean existByUserId(String userId);
+}
