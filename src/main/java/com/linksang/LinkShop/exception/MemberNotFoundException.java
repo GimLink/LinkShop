6fa6153bab7c4c2e4832f0ee6955a62ad5486239
Link <mexlove2@gmail.com>
2024-05-30 16:44:08 +0900

    예외 생성

---
+++ src/main/java/com/linksang/LinkShop/exception/MemberNotFoundException.java
@@ -0,0 +1,6 @@
+package com.linksang.LinkShop.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+
+    public MemberNotFoundException (String message) {super(message);}
+}
---
+++ src/main/java/com/linksang/LinkShop/exception/NotLoginException.java
@@ -0,0 +1,5 @@
+package com.linksang.LinkShop.exception;
+
+public class NotLoginException extends RuntimeException {
+    public NotLoginException(String message){super(message);}
+}
