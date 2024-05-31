528c35cfe44d775703eae6c6e53788c4e7b095c9
Link <mexlove2@gmail.com>
2024-05-31 16:44:05 +0900

    aws 설정 변경

--- src/main/java/com/linksang/LinkShop/config/AwsS3Config.java
+++ src/main/java/com/linksang/LinkShop/config/AwsS3Config.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accesskey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretkey}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
@@ -29,4 +29,6 @@ public class AwsS3Config {
                 .withRegion(region)
                 .build();
     }
+
+
 }
