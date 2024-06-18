91ef5468f488e73b07114e83466c07c20c4f6c46
Link <mexlove2@gmail.com>
2024-06-18 18:04:57 +0900

    프런트 이슈

---
+++ src/main/resources/static/assets/js/member/member_findId.js
@@ -0,0 +1,201 @@
+let x;
+$(function(){
+
+    let csrfToken = $("meta[name=_csrf]").attr("content");
+    let csrfHeader = $("meta[name=_csrf_header]").attr("content");
+
+    $("#phone1").on("keyup", function(){
+
+        var mch = $(this).val();
+        if( mch != null && mch.length == 3 )
+        {
+            $("#phone2").focus();
+        }
+    });
+
+    $("#phone2").on("keyup", function(){
+
+        var mch = $(this).val();
+        if( mch != null && mch.length == 4 )
+        {
+            $("#phone3").focus();
+        }
+    });
+
+    // 인증번호 전송
+    $("#sendMessage").on("click", function(){
+
+        let phone1 = $("#phone1").val();
+        let phone2 = $("#phone2").val();
+        let phone3 = $("#phone3").val();
+
+        if(phone1 == "" || phone2 == "" || phone3 == ""){
+            alert("입력하신 번호를 다시 확인해주세요.");
+            return false;
+        }
+
+        let result = confirm("입력하신 휴대폰 번호로 인증번호를 받으시겠습니까?");
+        if(result){
+            $.ajax({
+                url : "/member/findId/sendMessage",
+                type : "post",
+                data : { phone1 : phone1,
+                    phone2 : phone2,
+                    phone3 : phone3
+                },
+                beforeSend : function(xhr){
+                    xhr.setRequestHeader(csrfHeader, csrfToken);
+                }
+            }).done(function(result){
+                if(result === "success"){
+                    alert("인증번호를 발송했습니다. \n인증번호가 오지 않는 경우 정보를 다시 확인해 주세요. ");
+                    makeAuthInput();
+                    timer() ;
+                }else{
+                    alert(result);
+                    return false;
+                }
+            }).fail(function(result){
+                alert("에러가 발생했습니다. \n잠시후 다시 시도해주세요.");
+                return false;
+            });
+        }
+    });
+
+    // 인증번호 확인
+    $(document).on("click", "#checkAuthNum", function(){
+
+        let phone1 = $("#phone1").val();
+        let phone2 = $("#phone2").val();
+        let phone3 = $("#phone3").val();
+        let authNum = $("#authNum").val();
+
+        $.ajax({
+            url : "/member/findId/authNum",
+            type : "post",
+            data : { phone1 : phone1,
+                phone2 : phone2,
+                phone3 : phone3,
+                authNum : authNum
+            },
+            beforeSend : function(xhr){
+                xhr.setRequestHeader(csrfHeader, csrfToken);
+            }
+        }).done(function(result){
+            if(result === "success"){
+                alert("인증이 완료되었습니다.");
+                confirmSuccess();
+            }else if(result === "fail"){
+                alert("인증번호가 유효하지 않습니다.");
+                return false;
+            }else{
+                alert(result);
+                return false;
+            }
+        }).fail(function(result){
+            alert("에러가 발생했습니다. \n잠시후 다시 시도해주세요.");
+            return false;
+        });
+
+    });
+
+    // 아이디 확인 버튼
+    $(document).on("click", "#findId", function(){
+
+        $.ajax({
+            url : "/member/findId/findIdResult",
+            type : "post",
+            beforeSend : function(xhr){
+                xhr.setRequestHeader(csrfHeader, csrfToken);
+            }
+        }).done(function(result){
+            if(result == null){
+                alert("인증을 완료해주세요.");
+                return false;
+            }else{
+                $(".container-fluid").html(result);
+            }
+        }).fail(function(result){
+            alert("인증을 완료해주세요.");
+            return false
+        });
+    });
+
+});
+
+function makeAuthInput(){
+
+    let length = $(".input-wrap #authNum").length;
+    if(length == 0){
+        let html = "";
+        html += '<div class="flex-form">'
+        html += '<div class="flex-input-area">'
+        html += '<div class="input-wrap">'
+        html += '<input type="tel" id="authNum" maxlength="6" placeholder="6자리 입력">'
+        html += '<span class="countdown">00:00</span>'
+        html += '</div>'
+        html += '</div>'
+        html += '<div class="flex-btn-area">'
+        html += '<button type="button" id="checkAuthNum" class="btn btn-outline-black">확인</button>'
+        html += '</div>'
+        html += '</div>'
+
+        $(".certification-area").append(html);
+    }
+}
+
+
+function timer(){
+
+    let time = 180;
+    let min = "";
+    let sec = "";
+
+    let startTimer = function(){
+        x = setInterval(function(){
+            min = parseInt(time/60);
+            sec = time % 60;
+
+            $(".countdown").html(min + ":" + sec);
+            time--;
+
+            if(time < 0){
+                clearInterval(x);
+                x = null;
+                $(".countdown").html("00:00");
+                alert("유효시간이 종료되었습니다.");
+            }
+        }, 1000);
+    }
+
+    if(!x){
+        startTimer();
+    }else{
+        clearInterval(x);
+        startTimer();
+    }
+}
+
+function confirmSuccess(){
+
+    readOnly($("#phone1"));
+    readOnly($("#phone2"));
+    readOnly($("#phone3"));
+    readOnly($("#authNum"));
+
+    disabled($("#sendMessage"));
+    disabled($("#checkAuthNum"));
+
+    clearInterval(x);
+    $(".countdown").remove();
+}
+
+
+function readOnly(obj){
+    obj.attr("readonly", true);
+}
+
+function disabled(obj){
+    obj.attr("disabled", true);
+}
+
---
+++ src/main/resources/static/assets/js/member/member_mypage.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+
+    let token = $("meta[name=_csrf]").attr("content");
+    let header = $("meta[name=_csrf_header]").attr("content");
+
+    $(document).on("click", ".qna_content", function(){
+        if($(this).hasClass("qna_content_on")){
+            $(this).removeClass("qna_content_on");
+        }else{
+            $(this).addClass("qna_content_on");
+        }
+    });
+
+    // qna 더보기
+    $(document).on("click", "#qnamore", function(){
+
+        let lastQnAId = $("#lastQnAId").val();
+
+        $.ajax({
+            type : "get",
+            url  : "/mypage/qnaList?lastQnAId=" + lastQnAId +"&more=more"
+        }).done(function(result){
+            $("#lastQnAId").remove();
+            $(".myqna_list").append(result);
+        }).fail(function(){
+            alert("에러가 발생했습니다.");
+        });
+    });
+
+    // qna 전체 선택 or 전체 해제
+    $(document).on("click", ".qna_select_all", function(){
+
+        if($(this).is(":checked")){
+            $(".qna_select").prop("checked", true);
+        }else{
+            $(".qna_select").prop("checked", false);
+        }
+    });
+
+    // qna 보기
+    $("#mypageqna").on("click", function(){
+
+        $.ajax({
+            type : "get",
+            url  : "/mypage/qnaList"
+        }).done(function(result){
+            $(".orderpage").html(result);
+        }).fail(function(){
+            alert("에러가 발생했습니다.");
+        });
+
+    });
+
+    // qna 삭제
+    $(document).on("click", "#qnaDelete", function(){
+
+        let qnaIdList = new Array();
+
+        $(".qna_select").each(function(){
+            if($(this).is(":checked")){
+                let qna_content = $(this).closest(".qna_content");
+                qnaIdList.push(qna_content.find("input[name=qnaId]").val());
+            }
+        });
+
+        if(qnaIdList.length < 1){
+            alert("댓글을 선택해주세요");
+            return false;
+        }
+
+        let result = confirm("선택한 댓글들을 삭제 하시겠습니까?");
+        if(result){
+            $.ajax({
+                type : "delete",
+                url  : "/mypage/qna/delete",
+                data : { qnaIdList : qnaIdList },
+                beforeSend : function(xhr){
+                    xhr.setRequestHeader(header, token);
+                },
+                traditional: true
+            }).done(function(result){
+                alert("질문 삭제에 성공하셨습니다.");
+                location.reload();
+            }).fail(function(){
+                alert("에러가 발생했습니다. 잠시후에 다시 시도해보시기 바랍니다.");
+                return false;
+            });
+        }
+    });
+
+    // 주문상품 보기
+    $("#orderListBtn").on("click", function(){
+
+        $.ajax({
+            type : "get",
+            url  : "/mypage/orderList",
+        }).done(function(result){
+            $(".orderpage").html(result);
+        }).fail(function(){
+            alert("에러가 발생했습니다.");
+        });
+    });
+
+
+    // 주문상품 더보기 버튼
+    $(document).on("click", "#moreOrder", function(){
+
+        let lastOrderId = $("#lastOrderId").val();
+
+        $.ajax({
+            type : "get",
+            url  : "/mypage/orderList?lastOrderId=" + lastOrderId + "&more=more"
+        }).done(function(result){
+            $("#lastOrderId").remove();
+            $(".orderpage").append(result);
+        }).fail(function(){
+            alert("에러가 발생했습니다.");
+        });
+    });
+
+
+});
+
+
