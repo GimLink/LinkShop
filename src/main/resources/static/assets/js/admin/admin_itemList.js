f6df3193877c05094ef6f794d16c3dc05dddc420
Link <mexlove2@gmail.com>
2024-07-02 17:11:28 +0900

    프런트 이슈

---
+++ src/main/resources/static/assets/js/admin/admin_itemList.js
@@ -0,0 +1,88 @@
+$(function(){
+
+    let csrfToken = $("meta[name=_csrf]").attr("content");
+    let csrfHeader = $("meta[name=_csrf_header]").attr("content");
+
+    // 단일 상품 삭제
+    $(".item_delete").on("click", function(){
+
+        let idList = new Array();
+
+        let tr = $(this).closest("tr");
+        let itemId = tr.find("input[name=itemId]").val();
+        idList.push(itemId);
+
+        let result = confirm("해당 상품을 삭제 하시겠습니까? (한번이라도 구매된 상품은 삭제 불가능합니다.) ");
+        if(result){
+            $.ajax({
+                type : "delete",
+                url  : "/admin/item/delete",
+                data : { itemIdList : idList },
+                beforeSend : function(xhr){
+                    xhr.setRequestHeader(csrfHeader, csrfToken);
+                },
+                traditional: true
+            }).done(function(result){
+                if(result == "success"){
+                    alert("해당 상품을 삭제했습니다.");
+                    location.reload();
+                }
+            }).fail(function(){
+                alert("상품 삭제가 불가능합니다.");
+                return false;
+            });
+        }
+    });
+
+    // 상품 전체 선택 or 전체 해제
+    $(".checkbox-selectall").on("click", function(){
+
+        if($(this).is(":checked")){
+            $(".checkbox-select").prop("checked", true);
+        }else{
+            $(".checkbox-select").prop("checked", false);
+        }
+    });
+
+
+    // 선택 상품 삭제
+    $("#delete_select").on("click", function(){
+
+        let idList = new Array();
+
+        $(".checkbox-select").each(function(){
+            if($(this).is(":checked")){
+                let tr = $(this).closest("tr");
+                idList.push(tr.find("input[name=itemId]").val());
+            }
+        });
+
+        if(idList.length == 0){
+            alert("상품을 선택해주세요");
+            return false;
+        }
+
+        let result = confirm("선택한 상품들을 삭제 하시겠습니까?");
+        if(result){
+
+            $.ajax({
+                type : "delete",
+                url  : "/admin/item/delete",
+                data : { itemIdList : idList },
+                beforeSend : function(xhr){
+                    xhr.setRequestHeader(csrfHeader, csrfToken);
+                },
+                traditional : true
+            }).done(function(result){
+                if(result === "success"){
+                    location.reload();
+                }
+            }).fail(function(result){
+                alert("오류가 발생했습니다. 잠시후 다시 시도해보시기 바랍니다.");
+                return false;
+            });
+        }
+
+    });
+
+});
---
+++ src/main/resources/static/assets/js/admin/admin_registerItem.js
@@ -0,0 +1,54 @@
+$(function(){
+
+    let csrfToken = $("meta[name=_csrf]").attr("content");
+    let csrfHeader = $("meta[name=_csrf_header]").attr("content");
+
+    //파일 업로드시 썸네일
+    $("#upload_image").change(function(e){
+        const images = e.target.files;
+        $(".img-box").empty();
+        for(let i=0; i<images.length; i++){
+            const Reader = new FileReader();
+            Reader.readAsDataURL(images[i]);
+            Reader.onload = function(){
+                const img = '<img src="'+Reader.result +'" alt="사진">';
+                $('.img-box').append(img);
+            }
+        }
+    });
+
+    // 상품 등록
+    $(document).on("click", "#itemPost", function(){
+
+        let form = $("#itemForm")[0];
+        let formData = new FormData(form);
+
+        $.ajax({
+            type : "post",
+            url  : "/admin/item/register",
+            enctype : 'multipart/form-data',
+            data : formData,
+            contentType : false,
+            processData : false,
+            beforeSend : function(xhr){
+                xhr.setRequestHeader(csrfHeader, csrfToken);
+            }
+        }).done(function(result){
+            if(result === "success"){
+                alert("상품 등록에 성공하였습니다.")
+                window.location.replace("/");
+            }else if(result === "role"){
+                alert("관리자 권한이 필요합니다");
+                return false;
+            }else{
+                alert(result);
+                return false;
+            }
+        }).fail(function(result){
+            alert("에러가 발생하였습니다. \n잠시후 다시 시도해주세요.");
+            return false;
+        });
+    });
+
+});
+
