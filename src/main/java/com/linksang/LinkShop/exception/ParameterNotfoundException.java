c9e38de86f6cb00ba1a6b4231a1605bcd472a5ef
Link <mexlove2@gmail.com>
2024-07-09 18:50:05 +0900

    에러객체

---
+++ src/main/java/com/linksang/LinkShop/exception/ParameterNotfoundException.java
@@ -0,0 +1,8 @@
+package com.linksang.LinkShop.exception;
+
+public class ParameterNotfoundException extends RuntimeException{
+
+    public ParameterNotfoundException(String message) {
+        super(message);
+    }
+}
