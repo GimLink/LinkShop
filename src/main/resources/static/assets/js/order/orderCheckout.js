db73976c9e690e3a6442ffb0dc7753d4f34571b2
Link <mexlove2@gmail.com>
2024-07-09 18:49:11 +0900

    프런트이슈

---
+++ src/main/resources/static/assets/js/order/orderCheckout.js
@@ -0,0 +1,123 @@
+let token = $("meta[name='_csrf']").attr("content");
+let header = $("meta[name='_csrf_header']").attr("content");
+
+$(document).ready(function(){
+
+    //주문하기 버튼
+    $(document).on("click", "#order-button", function (){
+
+        //결제타입
+        let payMethod = $("input[name=paymethod]:checked").val();
+
+        //배송정보 (이름, 전화번호, 주소)
+        let addressAndPayMethod = {
+            customerName : $("#customerName").val(),
+            customerPhoneNum : $("#customerPhoneNum").val(),
+            recipientName : $("#recipientName").val(),
+            recipientPhoneNum : $("#recipientPhoneNum").val(),
+            zipcode : $("#zipcode").val(),
+            address : $("#address").val(),
+            detailAddress : $("#detailAddress").val(),
+            payMethod : payMethod
+        }
+
+        if(payMethod === '가상계좌'){
+            virtualAccount(addressAndPayMethod);
+        }else if (payMethod === '카카오페이') {
+            kakaoPay(addressAndPayMethod);
+        }
+
+    });
+});
+
+//카카오페이
+function kakaoPay(address) {
+
+    $.ajax({
+        url : "/kakaoPay/order",
+        type : "post",
+        data : address,
+        beforeSend : function(xhr){
+            xhr.setRequestHeader(header, token);
+        }
+    }).done(function (result) {
+        if (result === 'fail') {
+            alert("필수정보를 입력해주세요.");
+            return false;
+        }else if (result === "validation") {
+            alert("필수 값 혹은 정보를 정확하게 입력해주세요.");
+            return false;
+        } else {
+            window.location.href = result;
+        }
+    }).fail(function(result) {
+        alert("에러가 발생했습니다. \n 잠시 후 다시 시도해주세요.");
+        return false;
+    });
+}
+
+//가상계좌
+function virtualAccount(address) {
+
+    let tossPayments = TossPayments("test_ck_4yKeq5bgrpw1NDnqgBKxVGX0lzW6");
+    let paymentData ={
+        amiut : $("#totalPrice").val(),
+        orderId : $("#orderNum").val(),
+        orderName : $("#orderName").val(),
+        customerName : $("#customerName").val(),
+        successUrl : window.location.origin + "/success",
+        failUrl: window.location.origin + "/fail",
+    };
+
+    $.ajax({
+        url : "/order/virtualAccount",
+        type : "post",
+        data : address,
+        beforeSend : function(xhr) {
+            if (result === 'success') {
+                tossPayments.requestPayment("가상계좌", paymentData);
+            }else if (result === 'fail') {
+                alert("결제방법을 다시 확인해주세요.");
+                return false;
+            } else {
+                alert(result);
+                return false;
+            }
+        },
+        error : function () {
+            alert("필수 주문 정보를 입력해주세요.");
+            return false;
+        }
+    });
+}
+
+//배송지 생성
+
+function createAddress(){
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            let addr = ''; // 주소 변수
+            let extraAddr = ''; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            $("#zipcode").val(data.zonecode);
+            $("#address").val(addr);
+
+            // 상세주소 포커스
+            $("#detailAddress").focus();
+        }
+    }).open();
+
+}
+
